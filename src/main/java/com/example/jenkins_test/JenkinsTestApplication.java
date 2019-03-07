420fa146af67c24b4604f3658a6d55cb8eaa2814
yg <yonggun.joo@blucean.net>
2019-03-07 17:23:56 +0900

    test

--- src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
@@ -1,14 +1,18 @@
 package com.example.jenkins_test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsTestApplication {
 
+	private static Logger  log = LoggerFactory.getLogger(JenkinsTestApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
 		
+		log.info("111111111111111");
 	}
 
 }
