b47f6fdc557ff643ece7139caa6af2c3142d1dd6
yg <yonggun.joo@blucean.net>
2019-03-08 16:11:12 +0900

    test

--- src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
@@ -1,18 +1,25 @@
 package com.example.jenkins_test;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsTestApplication {
+public class JenkinsTestApplication implements CommandLineRunner{
 
 	private static Logger  log = LoggerFactory.getLogger(JenkinsTestApplication.class);
-	public static void main(String[] args) {
+	
+	public static void main(String[] args)  {
 		SpringApplication.run(JenkinsTestApplication.class, args);
-		
-		log.info("111111111111111");
 	}
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("CommandLineRunner Args: " + Arrays.toString(args));
+    }
 }
